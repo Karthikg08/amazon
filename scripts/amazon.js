3fba8aacbce515373d7e98ff333caf7121cd5888
Karthikg08 <kart03309@gmail.com>
2025-12-30 16:02:22 +0530

    separate js file for product data and remove data in amazon,js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,4 @@
-let products = [
-  {
-   
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
-  {
-    
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095,
-    keywords: [
-      "sports",
-      "basketballs"
-    ]
-  }];
+
   let productsHTML='';
 
   products.forEach(product => {
