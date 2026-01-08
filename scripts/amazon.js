079b6fbe9bae25afe5e8dd24f7cef9aa3129ec0d
Karthikg08 <kart03309@gmail.com>
2026-01-08 14:07:14 +0530

    i moved some code, because it belongs  there

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
   let productsHTML='';
@@ -57,37 +57,24 @@ import {products} from '../data/products.js';
 
   
 
+  function updateCartQuantity(){
+    let cartquantity = 0;
+      cart.forEach((cartItem) => {
+        cartquantity += cartItem.quantity;
+      })
+    document.querySelector('.js-cart-quantity').innerHTML = cartquantity;
+    console.log(cartquantity)
+  }
+
+
   document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
   document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
     button.addEventListener('click',() => {
         const productId = button.dataset.productId;
-
-        let matchingItem ;
-
-      cart.forEach((item) =>{
-        if (productId === item.productId){
-          matchingItem = item;
-        }
-      })
-      if (matchingItem){
-        matchingItem.quantity += 1;
-      } else{
-        cart.push({
-          productId: productId,
-          quantity:1
-        });
-      }
-      
-      
-      let cartquantity = 0;
-      cart.forEach((item) => {
-        cartquantity += item.quantity;
-      })
-      document.querySelector('.js-cart-quantity').innerHTML = cartquantity;
-
-
-        console.log(cartquantity)
+        addToCart(productId);
+        updateCartQuantity()
+     
         console.log(cart);
     });
   })
