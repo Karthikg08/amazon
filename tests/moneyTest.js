78c3dbc630bdb33ddf1662e5bc846906a4a77954
Karthikg08 <kart03309@gmail.com>
2026-02-20 19:51:20 +0530

    name the test cases : test suite: formatCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,5 +1,7 @@
 import {formatCurrency } from '../scripts/utils/money.js ';
 
+console.log('test suit: formatCurrency')
+
 if (formatCurrency(4359) === '43.59') {
   console.log(' it\'s working')
 } else {
