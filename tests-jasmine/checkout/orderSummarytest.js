a59bb531162f580ef83f2ce89a0c20a31a84d7e2
Karthikg08 <kart03309@gmail.com>
2026-03-11 18:36:35 +0530

    check another product successfully!

--- tests-jasmine/checkout/orderSummarytest.js
+++ tests-jasmine/checkout/orderSummarytest.js
@@ -7,14 +7,15 @@ describe('test suite: renderOrderSummary', () => {
     document.querySelector('.js-test-container').innerHTML =` 
       <div class="js-order-summary"></div> 
     `;
-    const product1 ="e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
+    const product1 ="e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+    const product2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId:product1,
         quantity: 2, 
         deliveryOptionId: '1'
       },{
-        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        productId:product2,
         quantity:1,
         deliveryOptionId: '2'
       }]);
@@ -29,5 +30,8 @@ describe('test suite: renderOrderSummary', () => {
     expect(
     document.querySelector(`.js-product-quantity-${product1}`).innerText
     ).toContain('Quantity: 2');
+     expect(
+    document.querySelector(`.js-product-quantity-${product2}`).innerText
+    ).toContain('Quantity: 1');
   });
 });
