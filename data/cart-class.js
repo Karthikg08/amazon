7b47c0c7a0078e982c9757ef1af1578c55d6c602
Karthikg08 <kart03309@gmail.com>
2026-03-16 21:10:19 +0530

    inventing constructot to the class

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,17 +2,24 @@ class Cart {
   cartItems = undefined; // shortcut 'cart;'
   localStorageKey = undefined;
 
-  loadFromStorage(){
-    this.cart =JSON.parse(localStorage.getItem(this.localStorageKey)) 
+  constructor(localStorageKey){
+    
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+   
+  }
 
-      if (!this.cart){
+  loadFromStorage(){
+    this.cartItems =JSON.parse(localStorage.getItem(this.localStorageKey)) 
+
+      if (!this.cartItems){
         this.cartItems =[{
           productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
           quantity: 2, 
           deliveryOptionId: '1'
         },{
           productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-          quantity:1,
+          quantity: 1,
           deliveryOptionId: '2'
         }];
       }
@@ -39,7 +46,7 @@ class Cart {
           //deliveryOptionId:'1'
         });
       }
-      saveToStorage();
+      this.saveToStorage();
   }
 
   removeFromCart(productId){
@@ -50,8 +57,8 @@ class Cart {
       }
     })  
 
-    this.cartItems = newCart;
-    saveToStorage();
+    this.cartItems = newCart;   
+    this.saveToStorage();
   }
 
   updateDeliveryOption(productId, deliveryOptionId){
@@ -63,7 +70,7 @@ class Cart {
         }
       })
       matchingItem.deliveryOptionId = deliveryOptionId;
-      saveToStorage();
+      this.saveToStorage();
 
   }
 
@@ -72,14 +79,9 @@ class Cart {
 
 
 
-const cart =new Cart();        
-const businessCart =new Cart();        
+const cart =new Cart('cart-oop');        
+const businessCart =new Cart('business-cart');        
 
-cart.loadFromStorage();
-businessCart.loadFromStorage();
-
-cart.localStorageKey = 'cart-oop';
-businessCart.localStorageKey = 'business-cart';
 
 console.log(cart); 
 console.log(businessCart); 
