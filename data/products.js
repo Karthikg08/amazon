be5069dce90f497a680c439dad89c90b946afbd1
Karthikg08 <kart03309@gmail.com>
2026-03-17 17:53:45 +0530

    changed the object  into class for more feature

--- data/products.js
+++ data/products.js
@@ -8,6 +8,40 @@ export function getProduct(productId) {
   });
   return matchingProduct;
 }
+class Products{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+
+  }
+
+}
+
+const product1 = new Products({
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  });
+console.log(product1);
 
 export const products = [
   {
@@ -668,4 +702,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Products(productDetails)
+});
+
+console.log(products);
