b5c49e587529eb6e82bf5fa59562ce20bb77c919
Karthikg08 <kart03309@gmail.com>
2026-03-18 15:00:45 +0530

    Inheritance : reuse code in class

--- data/products.js
+++ data/products.js
@@ -10,7 +10,7 @@ export function getProduct(productId) {
   });
   return matchingProduct;
 }
-class Products {
+class Product {
   id;
   image;
   name;
@@ -23,7 +23,7 @@ class Products {
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
-    
+
   }
 
   getStarsUrl() {
@@ -34,24 +34,52 @@ class Products {
     return `$${formatCurrency(this.priceCents)}`;
   }
 
+  extraInfoHTML() {
+    return '';
+  }
+
 }
 
-const product1 = new Products({
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+
+  }
+
+  extraInfoHTML() {
+    return`
+    <a href="${this.sizeChartLink}" target = "_blank">size chart</a>
+    `;
+
+  }
+
+}
+
+const tshirt = new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
       stars: 4.5,
-      count: 87
+      count: 56
     },
-    priceCents: 1090,
+    priceCents: 799,
     keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   });
-console.log(product1);
+
+  console.log(tshirt)
+  console.log(tshirt.getPrice())
+
+
 
 export const products = [
   {
@@ -713,7 +741,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
-  return new Products(productDetails)
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails)
 });
 
-console.log(products);
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -42,6 +42,8 @@ import { formatCurrency } from './utils/money.js';
             </select>
           </div>
 
+          ${product.extraInfoHTML()}
+
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
